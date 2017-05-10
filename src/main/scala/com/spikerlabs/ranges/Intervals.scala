898a60fe5e99de92b8b549b078ddc9436aaa047c
Arturas Smorgun <arturas@spikerlabs.com>
2017-05-10 01:54:59 +0100

    Implement sample and optimised ranges

---
+++ src/main/scala/com/spikerlabs/ranges/Intervals.scala
@@ -0,0 +1,79 @@
+package com.spikerlabs.ranges
+
+import scala.collection.mutable.SortedMap
+
+trait Ranges {
+  def countAt(num: Double): Int
+}
+
+// sample
+
+case class SampleRanges(r: List[(Double, Double)]) extends Ranges {
+  def countAt(num: Double): Int = {
+    r.count {
+      case (lower, higher) => lower <= num && num < higher
+    }
+  }
+}
+
+// optimised
+
+trait RangePoint {
+  def count: Int
+  def diff: Int
+}
+case class Start(count: Int = 1, diff: Int = 1) extends RangePoint
+case class End(count: Int = 0, diff: Int = -1) extends RangePoint
+
+case class OptimisedRanges private(r: SortedMap[Double, RangePoint]) extends Ranges {
+    def countAt(num: Double): Int = {
+      r.takeWhile {
+        case (key, _) => key <= num
+      }.lastOption.map {
+        case (_, point) => point.count
+      }.getOrElse(0)
+    }
+  }
+
+object OptimisedRanges {
+  def apply(r: List[(Double, Double)]): OptimisedRanges = {
+    def add(m: SortedMap[Double, RangePoint], location: Double, point: RangePoint) = {
+      if (m.contains(location)) {
+        m(location) match {
+          case existingPoint if existingPoint.diff + point.diff == 0 => m - location
+          case existingPoint: Start => m + (location -> existingPoint.copy(diff = existingPoint.diff + point.diff))
+          case existingPoint: End => m + (location -> existingPoint.copy(diff = existingPoint.diff + point.diff))
+        }
+      } else {
+        m + (location -> point)
+      }
+    }
+    def addAll(m: SortedMap[Double, RangePoint], points: List[(Double, RangePoint)]): SortedMap[Double, RangePoint] =
+      points match {
+        case Nil => m
+        case (key, point) :: tail => addAll(add(m, key, point), tail)
+      }
+    def index(m: SortedMap[Double, RangePoint], keys: List[Double], currentCount: Int = 0): SortedMap[Double, RangePoint] =
+      keys match {
+        case Nil => m
+        case key :: restOfKeys if m.contains(key) =>
+          m(key) match {
+            case point : Start => index(
+              m + (key -> point.copy(count = currentCount + point.diff)),
+              restOfKeys,
+              currentCount + point.diff
+            )
+            case point : End => index(
+              m + (key -> point.copy(count = currentCount + point.diff)),
+              restOfKeys,
+              currentCount + point.diff
+            )
+        }
+      }
+    val points = r.flatMap {
+      case (s, e) => List((s, Start()), (e, End()))
+    }
+    val draft = addAll(SortedMap.empty[Double, RangePoint], points)
+    OptimisedRanges(index(draft, draft.keys.toList))
+  }
+}
---
+++ src/test/scala/com/spikerlabs/ranges/IntervalsSpec.scala
@@ -0,0 +1,86 @@
+package com.spikerlabs.ranges
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class IntervalsSpec extends FlatSpec with Matchers {
+  "sample ranges" should "count dummy range" in {
+    val ranges = SampleRanges(List((1.0, 2.0), (1.0, 4.0), (7.0, 9.0), (1.0, 10.0), (3.0, 5.0)))
+    ranges.countAt(1.0) should be(3)
+    ranges.countAt(2.0) should be(2)
+    ranges.countAt(3.0) should be(3)
+    ranges.countAt(4.0) should be(2)
+    ranges.countAt(5.0) should be(1)
+    ranges.countAt(6.0) should be(1)
+    ranges.countAt(7.0) should be(2)
+    ranges.countAt(8.0) should be(2)
+    ranges.countAt(9.0) should be(1)
+    ranges.countAt(10.0) should be(0)
+  }
+  it should "count sample range" in {
+    val ranges = SampleRanges(List((0.0, 12.0), (106.0, 122.0), (123.0, 124.0), (11.0, 33.0), (33.0, 34.0)))
+    ranges.countAt(2.0) should be(1)
+    ranges.countAt(3.0) should be(1)
+    ranges.countAt(5.0) should be(1)
+    ranges.countAt(28.0) should be(1)
+    ranges.countAt(64.0) should be(0)
+    ranges.countAt(35.0) should be(0)
+    ranges.countAt(63.0) should be(0)
+    ranges.countAt(27.0) should be(1)
+    ranges.countAt(44.0) should be(0)
+    ranges.countAt(11.5) should be(2)
+    ranges.countAt(12.0) should be(1)
+    ranges.countAt(33.0) should be(1)
+  }
+  "optimised ranges" should "count at point matching exactly with start" in {
+    val ranges = OptimisedRanges(List((1.0, 2.0)))
+    ranges.countAt(1.0) should be(1)
+  }
+  it should "count when contains single range" in {
+    val ranges = OptimisedRanges(List((1.0, 3.0)))
+    ranges.countAt(0.5) should be(0)
+    ranges.countAt(1.0) should be(1)
+    ranges.countAt(2.0) should be(1)
+    ranges.countAt(3.0) should be(0)
+    ranges.countAt(3.5) should be(0)
+  }
+  it should "count when range has a subrange" in {
+    val ranges = OptimisedRanges(List((1.0, 10.0), (3.0, 8.0)))
+    ranges.countAt(2.0) should be(1)
+    ranges.countAt(5.0) should be(2)
+    ranges.countAt(9.0) should be(1)
+  }
+  it should "count when ranges intersect" in {
+    val ranges = OptimisedRanges(List((1.0, 5.0), (3.0, 7.0)))
+    ranges.countAt(2.0) should be(1)
+    ranges.countAt(4.0) should be(2)
+    ranges.countAt(6.0) should be(1)
+  }
+  it should "count dummy range" in {
+    val ranges = OptimisedRanges(List((1.0, 2.0), (1.0, 4.0), (7.0, 9.0), (1.0, 10.0), (3.0, 5.0)))
+    ranges.countAt(1.0) should be(3)
+    ranges.countAt(2.0) should be(2)
+    ranges.countAt(3.0) should be(3)
+    ranges.countAt(4.0) should be(2)
+    ranges.countAt(5.0) should be(1)
+    ranges.countAt(6.0) should be(1)
+    ranges.countAt(7.0) should be(2)
+    ranges.countAt(8.0) should be(2)
+    ranges.countAt(9.0) should be(1)
+    ranges.countAt(10.0) should be(0)
+  }
+  it should "count sample range" in {
+    val ranges = OptimisedRanges(List((0.0, 12.0), (106.0, 122.0), (123.0, 124.0), (11.0, 33.0), (33.0, 34.0)))
+    ranges.countAt(2.0) should be(1)
+    ranges.countAt(3.0) should be(1)
+    ranges.countAt(5.0) should be(1)
+    ranges.countAt(28.0) should be(1)
+    ranges.countAt(64.0) should be(0)
+    ranges.countAt(35.0) should be(0)
+    ranges.countAt(63.0) should be(0)
+    ranges.countAt(27.0) should be(1)
+    ranges.countAt(44.0) should be(0)
+    ranges.countAt(11.5) should be(2)
+    ranges.countAt(12.0) should be(1)
+    ranges.countAt(33.0) should be(1)
+  }
+}
